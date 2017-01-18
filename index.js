2de0a9a29193e1f981637f22eb0121557138bfc5
John Otander <johnotander@gmail.com>
2017-01-18 11:34:33 -0700

    Tweak colors add vibrancy

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-'use strict'
-
 const foregroundColor = '#222'
-const backgroundColor = '#ededed'
-const borderColor = '#eee'
+const backgroundColor = 'rgba(253, 253, 253, .8)'
+const tabBackgroundColor = 'rgba(240, 240, 240, .3)'
+const borderColor = '#f1f1f1'
 const cursorColor = 'rgb(244, 66, 173, .4)'
 
 const colors = {
@@ -36,29 +35,33 @@ module.exports.decorateConfig = config => Object.assign({}, config, {
 
     .tab_tab,
     .tabs_title {
+      opacity: .7;
       color: ${foregroundColor} !important;
+      background-color: ${tabBackgroundColor} !important;
+    }
+
+    .tab_tab.tab_active,
+    .tabs_title {
+      font-weight: 600;
     }
 
     .tab_tab.tab_active {
-      font-weight: 600;
-      color: ${foregroundColor} !important;
       background-color: ${backgroundColor} !important;
-    }
-
-    .tab_tab.tab_active::before {
-      border-bottom-color: ${backgroundColor} !important;
+      border-color: ${backgroundColor} !important;
     }
 
 		.term_fit:not(.term_term) {
 			opacity: .7;
-      background-color: '#f7f7f7';
 		}
 
-		.term_fit.term_active {
+		.term_fit.term_active,
+    .tab_tab.tab_active,
+    .tabs_title {
 			opacity: 1;
 			transition: opacity .1s ease-in-out;
 			will-change: opacity;
-      background-color: '#f7f7f7';
 		}
   `
 })
+
+module.exports.onWindow = browserWindow => browserWindow.setVibrancy('light')
