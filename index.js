728edfb08dcbcce0165507db6c1365199c693557
John Otander <johnotander@gmail.com>
2018-04-17 08:32:47 -0600

    Fix cursor color

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const foregroundColor = 'rgb(17, 17, 17)'
 const backgroundColor = 'rgb(255, 255, 255)'
 const tabBackgroundColor = 'rgb(255, 255, 255)'
 const borderColor = 'rgb(250, 250, 250)'
-const cursorColor = 'rgb(244, 66, 173, .4)'
+const cursorColor = 'rgba(244, 66, 173, .4)'
 
 module.exports.decorateConfig = config => Object.assign({}, config, {
 	cursorColor,
